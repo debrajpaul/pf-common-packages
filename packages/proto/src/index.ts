d4d81ff357f432ec6a3f8676f291f93ea4bfc9cb
Debraj Paul <debrajpaul@Debrajs-MacBook-Air.local>
2025-10-06 15:30:09 +0530

    feat: add protobuf contracts

--- packages/proto/src/index.ts
+++ packages/proto/src/index.ts
@@ -1,2 +1,7 @@
-/** @pf/proto — C-2 will add Buf, .proto files, and generated clients. */
-export const PROTO_READY = false;
+// Re-export generated clients & types for nicer imports
+export * as CommonV1 from "../gen/common/v1/context";
+export * as HealthV1 from "../gen/common/v1/health";
+export * as UserV1 from "../gen/user/v1/user";
+export * as TxnV1 from "../gen/transaction/v1/transaction";
+export * as BudgetV1 from "../gen/budget/v1/budget";
+export * as NotifyV1 from "../gen/notification/v1/notification";
